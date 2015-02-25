25113267896bba1a9cc4cfafb60db16eead229e1
eyalm <eyalm@wix.com>
2015-02-25 10:02:04 +0200

    clean code

--- app/scripts/jira-scrum.js
+++ app/scripts/jira-scrum.js
@@ -1,62 +1,55 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
-  setInterval(calc,3000);
-  calc();
+;(function (window, $, undefined) {
+  'use strict';
+  $( document ).ready(function() {
 
-  function calc(){
-    var totalBord = 0;
-    var columnsId = buildColums();
-    for(var i = 0;i<columnsId.length;i++){
-      var totalCol = sumCol(columnsId[i]);
-      totalBord = totalBord + totalCol;
-      var element =  $('.ghx-column[data-id='+columnsId[i]+']');
-      var totalEle = createTotalDisplay(totalCol,element);
-      //element.append(totalEle);
-    }
-    var totalEle = createTotalDisplay(totalBord,$('#ghx-board-name'));
-    //$('#ghx-board-name').append(totalEle)
-  }
-
-  function sumCol(colId){
-    var total = 0
-    $('.ghx-column[data-column-id='+colId+']').each( function () {
-      $(this).find('.aui-badge').each( function () {
-        var storyPoint = this.innerText;
-        if(isNumeric(storyPoint)) {
-          total = total + parseFloat (this.innerText);
-        }
-        console.log('value:' + total + ' storyPoint: ' + storyPoint);
-      })
-
-    });
-    return total;
-  }
-
-  function buildColums(){
-    var columnsIds = [];
-    $('.ghx-column').each( function () {
-      var dataId = $(this).attr('data-column-id');
-      if(columnsIds.indexOf(dataId) == -1 && dataId !== 'undefined'){
-        columnsIds.push(dataId);
+    calc();
+    function calc(){
+      var totalBord = 0;
+      var columnsId = buildColums();
+      for(var i = 0, len = columnsId.length; i< len; ++i){
+        var totalCol = sumCol(columnsId[i]);
+        totalBord = totalBord + totalCol;
+        var element =  $('.ghx-column[data-id='+columnsId[i]+']');
+        var totalEle = createTotalDisplay(totalCol);
+        element.append(totalEle);
       }
-      console.log('id:' +$(this).attr('data-column-id') + " lenght:"+ columnsIds.length);
-    });
-    return columnsIds;
-  }
-  // Utils
-  function createTotalDisplay(value,parent){
-    var element = parent.find('.aui-badge');
-    if(element.length > 0 ){
-      total = $(element[0]);
-    }else{
-      var total = $("<span class='aui-badge' style='font-size: 14px'/>")
-      parent.append(total);
+      var totalEle = createTotalDisplay(totalBord);
+      $('#ghx-board-name').append(totalEle)
     }
-    total.text(value);
-    return total;
-  }
-  function isNumeric(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-  }
 
-});
+    function sumCol(colId){
+      var total = 0
+      $('.ghx-column[data-column-id='+colId+']').each( function () {
+        $(this).find('.aui-badge').each( function () {
+          var storyPoint = this.textContent || this.innerText;
+          if(isNumeric(storyPoint)) {
+            total += parseFloat (this.innerText);
+          }
+        })
+
+      });
+      return total;
+    }
+
+    function buildColums(){
+      var columnsIds = [];
+      $('.ghx-column').each( function () {
+        var dataId = $(this).data('column-id');
+        if(columnsIds.indexOf(dataId) === -1 && dataId !== 'undefined'){
+          columnsIds.push(dataId);
+        }
+      });
+      return columnsIds;
+    }
+    // Utils
+    function createTotalDisplay(value){
+      var total = $('<span/>').addClass('aui-badge').css('font-size', '14px');
+      total.text(value);
+      return total;
+    }
+    function isNumeric(n) {
+      return $.isNumeric(n);
+    }
+
+  });  
+})(window, $);
